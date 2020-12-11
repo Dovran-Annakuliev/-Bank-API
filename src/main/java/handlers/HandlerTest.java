853699fe48000c7264fad8f5b065ad423a6c89c0
Dovran Annakuliev <55741979+Dovran-Annakuliev@users.noreply.github.com>
2020-12-11 14:17:29 +0300

    g

--- src/main/java/handlers/HandlerTest.java
+++ src/main/java/handlers/HandlerTest.java
@@ -13,6 +13,7 @@ import services.ServiceImpl;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
@@ -40,14 +41,20 @@ public class HandlerTest implements HttpHandler {
         String operation = jsonObject.getString("Operation");
         List<String> responce = null;
 
-        if (operation.equals("CreateNewCard"))
-            responce = createNewCard(jsonObject);
-        else if (operation.equals("GetAllCards"))
-            responce = getAllCards(jsonObject);
-        else if (operation.equals("Deposit"))
-            responce = deposit(jsonObject);
-        else if (operation.equals("GetBalance"))
-            responce = getBalance(jsonObject);
+        switch (operation) {
+            case "CreateNewCard":
+                responce = createNewCard(jsonObject);
+                break;
+            case "GetAllCards":
+                responce = getAllCards(jsonObject);
+                break;
+            case "Deposit":
+                responce = deposit(jsonObject);
+                break;
+            case "GetBalance":
+                responce = getBalance(jsonObject);
+                break;
+        }
 
         response(exchange, responce);
     }
@@ -75,15 +82,9 @@ public class HandlerTest implements HttpHandler {
 
     private List<String> getAllCards(JSONObject jsonObject){
         Long accountId = jsonObject.getLong("Account_id");
-        Long clientId = jsonObject.getLong("Client_id");
-        String accountNumber = jsonObject.getString("Account_number");
-        float balance = jsonObject.getFloat("Balance");
 
-        System.out.println("1");
         List<Card> list;
         list = service.getAllCards(accountDAO.findById(accountId));
-
-        System.out.println("sizeList = " + list.size());
 
         List<String> stringList = new LinkedList<>();
         for (Card c : list) {
@@ -125,6 +126,8 @@ public class HandlerTest implements HttpHandler {
         List<String> stringList = new LinkedList<>();
         stringList.add("" + service.getBalance(new Account(accountId, clientId, accountNumber, balance)));
 
+        System.out.println("size = " + stringList.size());
+
         System.out.println("responce:");
         for (String s : stringList)
             System.out.println(s);
@@ -133,20 +136,27 @@ public class HandlerTest implements HttpHandler {
     }
 
     private void response(HttpExchange exchange, List<String> response) throws IOException {
+
+        System.out.println("responce:");
+        for (String s : response)
+            System.out.println(s);
+
         System.out.println(response);
         exchange.sendResponseHeaders(200, response.size());
         OutputStream os = exchange.getResponseBody();
-        for (String s : response)
-            os.write(s.getBytes());
+        for (String s : response) {
+            os.write(s.getBytes(StandardCharsets.UTF_8));
+            os.flush();
+        }
         os.close();
     }
 
     private String IoToString(InputStream inputStream) {
-        String res = "";
+        StringBuilder res = new StringBuilder();
         Scanner scanner = new Scanner(inputStream);
         while (scanner.hasNext()){
-            res += scanner.next();
+            res.append(scanner.next());
         }
-        return res;
+        return res.toString();
     }
 }
